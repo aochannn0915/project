48302c6cb6dd50ba2a0892f2dc889331bfadb189
sorachann <soragram711@gmail.com>
2024-07-11 08:41:56 +0900

    change34

--- app/Http/Requests/OneRequest.php
+++ app/Http/Requests/OneRequest.php
@@ -24,20 +24,47 @@ class OneRequest extends FormRequest
     public function rules()
     {
         return [
-            'title' => 'required | max:255',
-            'url' => 'required | max:255 | url',
+            'mail_adress' => 'required | max:255',
+            'password'    => 'required | max:255',
+            'user_name'   =>'required | max:255',
+            'product_name'=>'required | max:255',
+            'company_name'=>'required | max:255',
+            'price'=>'required | max:255',
+            'stock'=>'required | max:255',
             'comment' => 'max:10000',
+        ];
+    }
+    public function attributes()
+    {
+        return [
+            'mail_adress' => 'メールアドレス',
+            'password'    => 'パスワード',
+            'user_name'   =>'ユーザーネーム',
+            'product_name'=>'商品名',
+            'company_name'=>'メーカー名',
+            'price'=>'値段',
+            'stock'=>'在庫',
+            'comment' => 'コメント',
         ];
     }
     public function messages(){
         return [
-            'mail_adress.required' => 'メールアドレスは入力は必須です。',
-            'password.required'    => 'パスワードは入力は必須です。',
-            'user_name'            => 'ユーザーネームは入力は必須です。',
-            'product_name'         => '商品名は入力は必須です。',
-            'company_name'         => '会社名は入力は必須です。',
-            'price'                => '値段は入力は必須です。',
-            'stock'                => '在庫は入力は必須です。',
+            'mail_adress.required' => ':attributeは入力は必須です。',
+            'mail_adress.max'      => ':attributeは:max字以内で入力してください。',
+            'password.required'    => ':attributeは入力は必須です。',
+            'password.max'         => ':attributeは:max字以内で入力してください。',
+            'user_name'            => ':attributeは入力は必須です。',
+            'user_name.max'        => ':attributeは:max字以内で入力してください。',
+            'product_name'         => ':attributeは入力は必須です。',
+            'product_name.max'     => ':attributeは:max字以内で入力してください。',
+            'company_name'         => ':attributeは入力は必須です。',
+            'company_name.max'     => ':attributeは:max字以内で入力してください。',
+            'price'                => ':attributeは入力は必須です。',
+            'price.max'            => ':attributeは:max字以内で入力してください。',
+            'stock'                => ':attributeは入力は必須です。',
+            'stock.max'            => ':attributeは:max字以内で入力してください。',
+            'comment'              => ':attributeは入力は必須です。',
+            'comment.max'          => ':attributeは:max字以内で入力してください。',
         ];
     }
 }
